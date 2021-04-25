b2638679e1a666e7a30cfe65323119e9c70f5fae
Andrei-Cristian Vizireanu <acvizireanu1@sheffield.ac.uk>
2021-04-25 19:31:19 +0100

    Changes for object avoidance in task1.py

--- src/task1.py
+++ src/task1.py
@@ -2,17 +2,75 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
+import numpy as np
 
-def callback(msg):
-    print msg.ranges[0]
-    move.linear.x = 0.1
-    if msg.ranges[0] < 1:
-        move.linear.x = 0
+class Tasks:
 
-    pub.publish(move)
+    def callback(self, msg):
+        self.move = Twist()
 
-rospy.init_node("check_obstacle")
-sub = rospy.Subscriber("/scan", LaserScan, callback)
-pub = rospy.Publisher("/cmd_vel", Twist)
-move = Twist()
-rospy.spin()
+        left_arc = msg.ranges[0:10]
+        right_arc = msg.ranges[-10:]
+        front_arc = np.array(left_arc + right_arc)
+        front_range = front_arc.min()
+
+
+        while front_range > 0.01:
+            left_arc = msg.ranges[0:10]
+            right_arc = msg.ranges[-10:]
+            front_arc = np.array(left_arc + right_arc)
+            front_range = front_arc.min()
+            print front_range
+            self.move.linear.x = 0.1
+            print self.move.linear.x
+
+        self.move.linear.x = 0
+        self.pub.publish(self.move)
+
+
+        #rospy.init_node("check_obstacle")
+        #sub = rospy.Subscriber("/scan", LaserScan, callback)
+        #pub = rospy.Publisher("/cmd_vel", Twist)
+        #move = Twist()
+        rospy.spin()
+
+
+    def __init__(self):
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        self.sub = rospy.Subscriber("/scan", LaserScan, self.callback)
+        rospy.init_node('publisher_node', anonymous=True)
+        self.rate = rospy.Rate(10) # hz
+        self.vel_cmd = Twist()
+        self.vel_cmd.linear.x = 0.1 # m/s
+        print ("init")
+
+        self.pub.publish(self.vel_cmd)
+
+        self.ctrl_c = False
+        rospy.on_shutdown(self.shutdownhook)
+
+        rospy.loginfo("publisher node is active...")
+
+
+
+    def shutdownhook(self):
+        self.shutdown_function()
+        self.ctrl_c = True
+
+    def shutdown_function(self):
+        print("stopping publisher node at: {}".format(rospy.get_time()))
+        self.vel_cmd.linear.x = 0.0 # m/s
+        self.pub.publish(self.vel_cmd)
+
+
+    def main_loop(self):
+        while not self.ctrl_c:
+            self.pub.publish(self.vel_cmd)
+            self.rate.sleep()
+
+if __name__ == '__main__':
+    publisher_instance = Tasks()
+    try:
+        publisher_instance.main_loop()
+    except rospy.ROSInterruptException:
+        pass
