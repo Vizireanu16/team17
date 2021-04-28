3a532d23a48bdf6a01e0f9cfced17c823b48446a
Chigozim Mogbolu <cumogbolu1@sheffield.ac.uk>
2021-04-28 08:58:51 +0100

    Chigozim commit

---
+++ src/task1_Gogo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+import numpy as np
+
+class Task1Gogo():
+#robot should search environment every 0.1s
+    def callback(self, data);
+    
+
+#robot should turn 90 degrees when obstacle is spotted
