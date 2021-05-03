8c7677b534be6e9da16d2b6746f7444d230fecc2
Hao-Hsuan Teng <hteng2@sheffield.ac.uk>
2021-05-03 21:56:17 +0100

    adding stuff to object detection.py

--- src/object_detection.py
+++ src/object_detection.py
@@ -2,6 +2,8 @@
 
 # Import the core Python modules for ROS and to implement ROS Actions:
 import rospy
+import time
+import math
 
 # Import some image processing modules:
 import cv2
@@ -13,6 +15,9 @@ from sensor_msgs.msg import Image
 # Import some other modules from within this package
 from move_tb3 import MoveTB3
 from tb3_odometry import TB3Odometry
+import numpy as np
+
+
 
 class colour_search(object):
 
@@ -22,6 +27,8 @@ class colour_search(object):
         self.camera_subscriber = rospy.Subscriber("/camera/rgb/image_raw",
             Image, self.camera_callback)
         self.cvbridge_interface = CvBridge()
+
+        self.status = 0
 
         self.robot_controller = MoveTB3()
         self.turn_vel_fast = -0.5
@@ -38,6 +45,19 @@ class colour_search(object):
         
         self.m00 = 0
         self.m00_min = 10000
+
+        self.mask = np.zeros((1080,1920,1), np.uint8)
+        self.lower_bound = []
+        self.upper_bound = []
+
+        self.color_boundaries = {
+            "red":    ([0, 200, 100], [8, 255, 255]),
+            "blue":   ([115, 224, 100],   [130, 255, 255]),
+            "yellow": ([28, 180, 100], [32, 255, 255]),
+            "green":   ([40, 50, 100], [65, 255, 255]),
+            "lightblue":   ([75, 50, 100], [90, 255, 255]),
+            "purple":   ([145, 185, 100], [150, 250, 255])
+        }
 
     def shutdown_ops(self):
         self.robot_controller.stop()
@@ -58,96 +78,105 @@ class colour_search(object):
 
         crop_img = cv_img[crop_y:crop_y+crop_height, crop_x:crop_x+crop_width]
         hsv_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
-
-        darkblue_lower = (115, 224, 100)
-        darkblue_upper = (130, 255, 255)
         
-        lightblue_lower = (87, 130, 100)
-        lightblue_upper = (91, 254, 255)
+        self.mask = cv2.inRange(hsv_img, np.array(self.lower_bound[0]), np.array(self.upper_bound[0]))
 
-        green_lower = (58, 185, 100)
-        green_upper = (61, 256, 255)
-
-        red_lower = (-1.8, 217, 100)
-        red_upper = (3.3, 255, 255)
-        
-        """Dark blue"""
-        mask_db = cv2.inRange(hsv_img, darkblue_lower, darkblue_upper)
-        res_db = cv2.bitwise_and(crop_img, crop_img, mask = mask_db)
-        m_db = cv2.moments(mask_db)
+        m = cv2.moments(self.mask)
         self.m00 = m_db['m00']
         self.cy = m_db['m10'] / (m_db['m00'] + 1e-5)
+
         if self.m00 > self.m00_min:
             cv2.circle(crop_img, (int(self.cy), 200), 10, (0, 0, 255), 2)
             cv2.imshow('cropped image', crop_img)
             cv2.waitKey(5)
-            print ("Dark blue")
-        """light blue"""
-        mask_lb = cv2.inRange(hsv_img, lightblue_lower, lightblue_upper)
-        res_lb = cv2.bitwise_and(crop_img, crop_img, mask = mask_lb)
-        m_lb = cv2.moments(mask_lb)
-        self.m00 = m_lb['m00']
-        self.cy = m_lb['m10'] / (m_lb['m00'] + 1e-5)
-        if self.m00 > self.m00_min:
-            cv2.circle(crop_img, (int(self.cy), 200), 10, (0, 0, 255), 2)
-            cv2.imshow('cropped image', crop_img)
-            cv2.waitKey(5)
-            print ("light blue")
-        """green"""
-        mask_g = cv2.inRange(hsv_img, green_lower, green_upper)
-        res_g = cv2.bitwise_and(crop_img, crop_img, mask = mask_g)
-        m_g = cv2.moments(mask_g)
-        self.m00 = m_g['m00']
-        self.cy = m_g['m10'] / (m_g['m00'] + 1e-5)
-        if self.m00 > self.m00_min:
-            cv2.circle(crop_img, (int(self.cy), 200), 10, (0, 0, 255), 2)
-            cv2.imshow('cropped image', crop_img)
-            cv2.waitKey(5)
-            print ("green")
-        """red"""
-        mask_r = cv2.inRange(hsv_img, red_lower, red_upper)
-        res_r = cv2.bitwise_and(crop_img, crop_img, mask = mask_r)
-        m_r = cv2.moments(mask_r)
-        self.m00 = m_r['m00']
-        self.cy = m_r['m10'] / (m_r['m00'] + 1e-5)
-        if self.m00 > self.m00_min:
-            cv2.circle(crop_img, (int(self.cy), 200), 10, (0, 0, 255), 2)
-            cv2.imshow('cropped image', crop_img)
-            cv2.waitKey(5)
-            print ("red")
+
+
+        
+        
+    def find_colour(self):
+        for color_name, (lower, upper) in self.color_boundaries.items():
+            lower_bound = np.array(lower)
+            upper_bound = np.array(upper)
+            mask = cv2.inRange(hsv_img, self.lower_bound, self.upper_bound)
+            if mask.any():
+                print("SEARCH INITIATED: The target colour is {}".format (color_name))
+                self.lower_bound = lower_bound
+                self.upper_bound = upper_bound
+
             
+    def turn_180(self):
+        time.sleep(1)
+        self.robot_controller.set_move_cmd(0.0, 0.55)    
+        self.robot_controller.publish()
+        print "turn left"
+        time.sleep(3)
+
+    def turn_back(self):
+        self.robot_controller.set_move_cmd(0.0, -0.56)    
+        self.robot_controller.publish()
+        print "turn right"
+        time.sleep(3)
+        self.robot_controller.set_move_cmd(0.0, 0.0)    
+        self.robot_controller.publish()
+        print "stop"
+
+
+    def find_pillar(self):
+        self.robot_controller.set_move_cmd(0.33, 0.0)    
+        self.robot_controller.publish()
+        print "move forward"
+        time.sleep(3)
+        self.robot_controller.set_move_cmd(0.0, 0.4)    
+        self.robot_controller.publish()
+        print "turn left"
+        time.sleep(7)
+        self.robot_controller.set_move_cmd(0.0, 0.0)    
+        self.robot_controller.publish()
+        print "stop"
+
 
     def main(self):
         while not self.ctrl_c:
-            if self.stop_counter > 0:
-                self.stop_counter -= 1
 
-            if self.m00 > self.m00_min:
-                # blob detected
-                if self.cy >= 560-100 and self.cy <= 560+100:
-                    if self.move_rate == 'slow':
-                        self.move_rate = 'stop'
-                        self.stop_counter = 30
+            if self.status == 0:
+                self.turn_180()
+                self.status+=1
+            elif self.status == 1:
+                self.find_colour()
+                self.status+=1
+            elif self.status == 2:
+                self.turn_back()
+                self.status+=1
+            elif self.status == 3:
+                self.find_pillar()
+                self.status+=1
+            elif self.status == 4:
+                if self.m00 > self.m00_min:
+                    if self.cy >= 560-100 and self.cy <= 560+100:
+                        if self.move_rate == 'slow':
+                            self.move_rate = 'stop'
+                            self.stop_counter = 30
+                        else:
+                            self.move_rate = 'slow'
+                    else:
+                        self.move_rate = 'fast'                
+                if self.move_rate == 'fast':
+                    print("MOVING FAST: I can't see anything at the moment, scanning the area...")
+                    self.robot_controller.set_move_cmd(0.0, self.turn_vel_fast)
+                elif self.move_rate == 'slow':
+                    print("MOVING SLOW: A blob of colour of size {:.0f} pixels is in view at y-position: {:.0f} pixels.".format(self.m00, self.cy))
+                    self.robot_controller.set_move_cmd(0.0, self.turn_vel_slow)
+                elif self.move_rate == 'stop' and self.stop_counter > 0:
+                    print("STOPPED: The blob of colour is now dead-ahead at y-position {:.0f} pixels... Counting down: {}".format(self.cy, self.stop_counter))
+                    self.robot_controller.set_move_cmd(0.0, 0.0)
                 else:
-                    self.move_rate = 'slow'
-            else:
-                self.move_rate = 'fast'
-                
-            if self.move_rate == 'fast':
-                #print("MOVING FAST: I can't see anything at the moment, scanning the area...")
-                self.robot_controller.set_move_cmd(0.0, self.turn_vel_fast)
-            elif self.move_rate == 'slow':
-                #print("MOVING SLOW: A blob of colour of size {:.0f} pixels is in view at y-position: {:.0f} pixels.".format(self.m00, self.cy))
-                self.robot_controller.set_move_cmd(0.0, self.turn_vel_slow)
-            elif self.move_rate == 'stop' and self.stop_counter > 0:
-                #print("STOPPED: The blob of colour is now dead-ahead at y-position {:.0f} pixels... Counting down: {}".format(self.cy, self.stop_counter))
-                self.robot_controller.set_move_cmd(0.0, 0.0)
-            else:
-                #print("MOVING SLOW: A blob of colour of size {:.0f} pixels is in view at y-position: {:.0f} pixels.".format(self.m00, self.cy))
-                self.robot_controller.set_move_cmd(0.0, self.turn_vel_slow)
+                    print("MOVING SLOW: A blob of colour of size {:.0f} pixels is in view at y-position: {:.0f} pixels.".format(self.m00, self.cy))
+                    self.robot_controller.set_move_cmd(0.0, self.turn_vel_slow)
             
-            self.robot_controller.publish()
-            self.rate.sleep()
+                self.robot_controller.publish()
+                self.rate.sleep()
+
+            
             
 if __name__ == '__main__':
     search_ob = colour_search()
